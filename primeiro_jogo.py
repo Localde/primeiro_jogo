21a957a9b20e5ba0015ff7a208f1607e79c761a7
Ollywer <dyegoollywer@gmail.com>
2020-01-05 11:03:21 -0300

    Adição do CREATE RECTANGLE, TEXT

--- primeiro_jogo.py
+++ primeiro_jogo.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from constantes import *
+import random
 
 class Jogo(object):
     """
@@ -17,18 +18,33 @@ class Jogo(object):
         self.frame.pack()
 
         #Criamos a tela do jogo
-        self.canvas = Canvas(self.frame, bg='black', width=CANVAS_L, height=CANVAS_A, cursor='watch')
+        self.canvas = Canvas(self.frame, bg='black', width=CANVAS_L, height=CANVAS_A, cursor='target')
         self.canvas.pack()
-
-        self.canvas.create_line((0, 0), 100, 100, fill='white')
 
         #E colocamos um botão para começar o jogo
         self.começar = Button(self.root, text='START')
         self.começar.pack()
 
-        #self.novoJogo()
+        self.novoJogo()
 
         self.root.mainloop()
 
+    def novoJogo(self):
+        """
+        Cria os elementos de um novo jogo
+        """
+        self.canvas.create_rectangle((CANVAS_L//2, 350), (CANVAS_L//2 + 100, 370), fill='green')
+
+        #Lista dos ratângulos
+        self.r = []
+
+        #E por fim as diversas fileiras de retângulos
+        l, c, e = 5, 8, 2 #Linhas, colunas e espaçamento
+        b, h, y0 = 48, 20, 50 #Base, altura e posição inicial dos retângulos
+        for i in range(l):
+            cor = random.choice(['green', 'orange', 'white', 'lightgray', 'yellow', 'purple'])
+            for j in range(c):
+                self.canvas.create_rectangle(b*j+(j+1)*e, i*h+(i+1)*e + y0, b*j+(j+1)*e+b, i*h+(i+1)*e + y0 + h, fill=cor)
+        self.canvas.create_text(CANVAS_L/2, CANVAS_A/2, text='OLA COLEGA!', fill='white')
 if __name__ == '__main__':
     Jogo()
